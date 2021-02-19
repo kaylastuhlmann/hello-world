441ed491ee71145a8513d07992dc94dc199a850c
kathleenkemp <kkemp@bowdoin.edu>
2021-02-19 11:31:00 -0500

    Changed hello world greeting

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,7 @@ Team members: Lisa Mekonnen, Kayla Stuhlmann, and Kathleem Kemp
 """
 
 def main():
-    print("Hello, world! xx")
+    print("Hello, Kayla and Lisa! It's Kathleen.")
 
 if __name__ == "__main__":
     main()
